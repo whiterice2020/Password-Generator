3c4de10a11063b4ec3a55da2f8189b5c15eac850
Paul DelPadre <PDelPadre@rice.local>
2020-06-10 16:46:46 -0400

    Generating Code

--- script.js
+++ script.js
@@ -5,17 +5,28 @@ var generateBtn = document.querySelector("#generate");
 // code to gen the password
 //research comcat, push, 
 function generatePassword(){
-
+// Setting Variables
     var numOfChar = prompt("How many Characters");
-    var upperCase = confirm("Do you want Uppercase Letters?");
-    var lowerCase = confirm("Do you want Lowercase Letters");
-    var specialCharacters = confirm("Do you want Special Characters?");
-    
+    var userUpperCase = confirm("Do you want Uppercase Letters?");
+    var userSpecialCharacters = confirm("Do you want Special Characters?");
+    var userNumbers = confirm("Do you want Numbers?");
+    var userLowerCase = confirm("Do you want Lowercase Letters");
+    var lowerChar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var num = '0123456789'; 
+    var sym = '!@#$%^&*=-_';
+    var upperCharArray=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    var lowerCharArray=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    // var numArray=[1,2,3,4,5,6,7,8,9,10]
+    var specialCharactersArray=["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "_", "-" ]
+
+    // Take Users Choices and put them into pw generator
 
 
+    console.log(num);
 
 
-  return numOfChar
+  return getRandomLower
   // return "This is where the results are of the code will be displayed"
 }
 
